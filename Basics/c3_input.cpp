aaec2562b9a4696f35e187ca11f833a5ebc6b028
BigBrainBoy1 <jiheershu1@gmail.com>
2024-01-08 22:29:30 +0530

    regular changes

---
+++ Basics/c3_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name, age;
+
+    cout << "Enter your name and age: ";   
+    cin >> name;
+    cin >> age;
+
+    cout << "Hello " << name  <<" "<< age;
+    return 0;
+}
