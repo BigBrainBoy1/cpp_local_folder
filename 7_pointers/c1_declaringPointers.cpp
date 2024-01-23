87fe25862741d2185175cd258009049303d3edb0
BigBrainBoy1 <jiheershu1@gmail.com>
2024-01-23 22:28:25 +0530

    files added for GE

--- 7_pointers/c1_declaringPointers.cpp
+++ 7_pointers/c1_declaringPointers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int a = 10;
-    int *ptr1{}; // we declare a pointer by using a * before variable name and empty curly braces represnt that this pointer is not pointing to anything yet
+    int *ptr1 = nullptr; // we declare a pointer by using a * before variable name and empty curly braces represnt that this pointer is not pointing to anything yet
     // to store the adress of an int variable we need a pointer of int variable i. e. pointer should be of the same data type as of the data type of the variable of whose adress is to be stored in it
     ptr1 = &a; // now ptr1 will store the adress of a in memory
     cout << ptr1 << endl;
---
+++ 7_pointers/c4_dynamicMemoryAllocation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int* ptr1 = nullptr; // good practice to initialize pointers with a null pointer so that it won't be pointing to anything when we use it
+    ptr1 = new int; // this will dynamically allocate enough memory to store variables of data type int for ptr1 and this memory belongs to our program now and the system can't use it for anything else until we return it
+    ptr1 = &a;
+    delete ptr1; // this will free the memory allocated to ptr1 and will be returned back to the os
+    ptr1 = nullptr; // again a good practice to set the pointer to nullptr after deleting it
+    /*
+    some NON - VALID commands below
+    int* ptr2 = nullptr;
+    *ptr2 = 20; // BAD
+    delete ptr2;
+    delete ptr2; // BAD to delete twice
+    */
+    return 0;
+}
