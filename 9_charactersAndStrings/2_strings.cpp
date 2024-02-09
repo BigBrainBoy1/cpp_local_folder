93ae2b28abec208f02ef87fa9965e6b39d9ed269
BigBrainBoy1 <jiheershu1@gmail.com>
2024-02-09 22:26:36 +0530

    regular changes

---
+++ 9_charactersAndStrings/2_strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// #include <string>
+
+using namespace std;
+
+int main()
+{
+    string str1 = {"qwe asd"};
+    cout << "Enter your name : ";
+    cin >> str1;
+    cout << str1;
+    return 0;
+}
