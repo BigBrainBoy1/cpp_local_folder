7959bfdd6385b129620e768b0936112c86161bc0
BigBrainBoy1 <jiheershu1@gmail.com>
2024-02-20 22:25:46 +0530

    regular changes

--- C++_practice/c2_.cpp
+++ C++_practice/c2_.cpp
@@ -2,24 +2,12 @@
 
 using namespace std;
 
-bool func1(int arr1[], int size)
-{
-    int count = 0;
-    for (int i = 0; i < (size - 1); i++)
-    {
-        if (arr1[i] == arr1[i + 1] && arr1[i + 1] == arr1[i + 1])
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
 int main()
 {
-    int arr1[] = {1, 1, 1, 2, 2, 2, 1}, size = 0;
-    size = ((sizeof(arr1)) / sizeof(arr1[0]));
-    cout << func1(arr1, size) << endl;
-    // func1(arr1, size);
+    int arr1[] = {}, size = 0;
+    size = (sizeof(arr1) / sizeof(arr1[0]));
+    for (int i = 0; i < size; i++)
+    {
+    }
     return 0;
 }
--- C++_practice/secQ1.py
+++ C++_practice/secQ1.py
@@ -1,22 +1,22 @@
 # Jiheershu     MAT/22/66       Bsc.(H) Mathematics
 
 # Q1 (a)
-listOfAges = []
+listOfAges = [] # initializing an empty list
 for i in range (10):
     print("Enter age of student ", i+1)
-    x = int(input())
-    listOfAges.append(x)
+    x = int(input()) # taking input of age
+    listOfAges.append(x) # storing age in the list
 sumOfAges = 0
 for i in range(10):
-    sumOfAges += listOfAges[i]
-avgAge = ((sumOfAges)/10)
-print("Average age among ", listOfAges, " is ", avgAge)
+    sumOfAges += listOfAges[i] # calculating the sum of ages to calculate average age
+avgAge = ((sumOfAges)/10) # calculating average age
+print("Average age among ", listOfAges, " is ", avgAge) # printing average age
 
 # Q1 (b)
 fruits = ["Apple", "Mango", "Peach", "Banana"]
 prices = [100, 80, 150, 70]
-dict1 = {}
+dict1 = {} # initializing an empty dictionary
 for keys in fruits:
       for values in prices:
-            dict1[keys] = values
-print("Resultant dictionary : ", dict1)
+            dict1[keys] = values # entering value in dictionary where the keys are from the list 'fruits' and values are from the list 'prices'
+print("Resultant dictionary : ", dict1) # printing the dictionary
