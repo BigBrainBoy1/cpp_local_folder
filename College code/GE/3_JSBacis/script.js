bcc817c8cefe8db6987dd77d8ce6f0de69c4c659
BigBrainBoy1 <jiheershu1@gmail.com>
2024-04-06 22:31:31 +0530

    JS practical work

--- College code/GE/3_JSBacis/script.js
+++ College code/GE/3_JSBacis/script.js
@@ -1,7 +1,17 @@
-function f1() {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-    let num3 = parseFloat(document.getElementById("num3").value);
-    let maxNum = Math.max(num1, num2, num3);
-    document.write("Max num out of ", num1, ", ", num2, " and ", num3, " is ", maxNum);
+// function f1() {
+//     let num1 = parseFloat(document.getElementById("num1").value);
+//     let num2 = parseFloat(document.getElementById("num2").value);
+//     let num3 = parseFloat(document.getElementById("num3").value);
+//     let maxNum = Math.max(num1, num2, num3);
+//     document.write("Max num out of ", num1, ", ", num2, " and ", num3, " is ", maxNum);
+// }
+
+
+
+
+var str1 = "";
+var a = 10;
+for (let i = 0; i <= 5; i++) {
+    str1 += `${a}\n`;
 }
+console.log(str1);
